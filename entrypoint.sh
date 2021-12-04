6ca6da0a156456b8615bc7861d59c79fff2c20ca
elliotxx <951376975@qq.com>
2021-12-04 17:08:23 +0800

    feat: add .github/workflows, .goreleaser.yml etc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -l
 set -e
 
-APP_NAME="go-cli-prototype"
+APP_PATH="/app/go-cli-prototype"
 
 showVersion=$1
 echoInfo=$2
 params=""
 
 if [ "$showVersion" = "true" ]; then
-    params+="-V "
+    params="$params -V"
 fi
 
-$APP_NAME $params $echoInfo
+$APP_PATH $params $echoInfo
